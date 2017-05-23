4cf3c6568375061ce5f940262cf1d4b4fb9d39f9
Aga Sadeq MD Shamsul Arefin <shamsul.arefin@infolytx.com>
2017-05-23 13:18:35 +0600

    learn threshold

--- notebooks/amazon_forest_notebook.py
+++ notebooks/amazon_forest_notebook.py
@@ -105,7 +105,7 @@ for i, (image_name, label) in enumerate(zip(images_title, labels_set)):
 
 img_resize = (64, 64) # The resize size of each image
 validation_split_size = 0.2
-epochs = 5
+epochs = 50
 batch_size = 128
 
 
